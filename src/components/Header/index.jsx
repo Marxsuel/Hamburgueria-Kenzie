82b730960bfad4a740577ae7d0910c8877fd7a6f
Marxsuel <charles.marx98@gmail.com>
2023-10-10 20:54:14 -0300

    Feat: UseEffect implementado, estilos criados

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Logo from "../../assets/Logo.svg";
 import { MdSearch, MdShoppingCart } from "react-icons/md";
+import styles from "./style.module.scss"
 
 export const Header = () => {
    const [value, setValue] = useState("");
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CartModal } from "../../components/CartModal";
 import { Header } from "../../components/Header";
 import { ProductList } from "../../components/ProductList";
@@ -8,17 +8,21 @@ export const HomePage = () => {
    const [productList, setProductList] = useState([]);
    const [cartList, setCartList] = useState([]);
 
-   const getProduct = async () => {
-      try {
-         const {data} = await productApi.get("products")
-         setProductList(data)
-         console.log(setProductList)
-      } catch (error) {
-         console.log(error)
-      }
 
+   useEffect(() => {
+      const getProduct = async () => {
+         try {
+            const { data } = await productApi.get("/products")
+            setProductList(data)
+            console.log(setProductList)
+         } catch (error) {
+            console.log(error)
+         }
+      }
       getProduct()
-   }
+   }, [])
+
+
 
 
    // useEffect montagem - carrega os produtos da API e joga em productList
